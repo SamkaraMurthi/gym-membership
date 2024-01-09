6c800d372bbc6a2ff800f483b9309e6ce59b83ce
[SamkaraMurti] <naranara318@gmail.com>
2024-01-09 13:21:37 +0100

    database changes

--- SQL_Narayana/studentdatabase.sql
+++ SQL_Narayana/studentdatabase.sql
@@ -1,16 +1,16 @@
 CREATE DATABASE studentdatabase;
-CREATE TABLE studentdata_nm
+CREATE TABLE studentdata_kn
 
 (
     id int not null auto_increment unique,
-    firstname_nm varchar(100),
-    lastname_nm varchar(100),
-    studentid_nm varchar(100),
-    yearofstudy_nm smallint,
-    birthplace_nm varchar(100),
+    firstname_kn varchar(100),
+    lastname_kn varchar(100),
+    studentid_kn varchar(100),
+    yearofstudy_kn smallint,
+    birthplace_kn varchar(100),
     primary key (id)
 );
-INSERT INTO studentdata_nm(id, firstname_nm,lastname_nm,studentid_nm,yearofstudy_nm,birthplace_nm)
+INSERT INTO studentdata_kn(id, firstname_kn,lastname_kn,studentid_kn,yearofstudy_kn,birthplace_kn)
 VALUES
 (1,"Nishan","Mukhopadhyay","60850",1,"India"),
 (2,"Inji","Budiman","65430",2,"Azerbaijian"),
@@ -19,13 +19,13 @@ VALUES
 (5,"Sapna","Sharma","54787",2,"India"),
 (6,"King","Ruther","65478",1,"Delhi")
 
-ALTER TABLE studentdata_nm ADD UNIQUE (studentid_kn);
+ALTER TABLE studentdata_kn ADD UNIQUE (studentid_kn);
 
-CREATE TABLE parents_nm
+CREATE TABLE parents_kn
 (
     id int not null auto_increment unique,
-    parentsname_nm varchar(100),
-    parentsage_nm varchar(100),
-    occupation_nm varchar(100),
+    parentsname_kn varchar(100),
+    parentsage_kn varchar(100),
+    occupation_kn varchar(100),
     primary key (id)
 );
